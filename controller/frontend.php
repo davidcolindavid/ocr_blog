a17846f6ca70476554a1a5c14b9f649f15a0aa13
davidcolin <davidcolin@live.fr>
2019-02-21 16:25:36 +0100

    Affiche la page d'un billet et ses commentaires

--- controller/frontend.php
+++ controller/frontend.php
@@ -2,6 +2,7 @@
 
 // Chargement des classes
 require_once('model/PostManager.php');
+require_once('model/CommentManager.php');
 
 function listPosts()
 {
@@ -9,4 +10,29 @@ function listPosts()
     $posts = $postManager->getPosts(); // Appel d'une fonction de cet objet
 
     require('view/frontend/listPostsView.php');
+}
+
+function post()
+{
+    $postManager = new \OpenClassrooms\Blog\Model\PostManager();
+    $commentManager = new \OpenClassrooms\Blog\Model\CommentManager();
+
+    $post = $postManager->getPost($_GET['id']);
+    $comments = $commentManager->getComments($_GET['id']);
+
+    require('view/frontend/postView.php');
+}
+
+function addComment($postId, $author, $comment)
+{
+    $commentManager = new \OpenClassrooms\Blog\Model\CommentManager();
+
+    $affectedLines = $commentManager->postComment($postId, $author, $comment);
+
+    if ($affectedLines === false) {
+        throw new Exception('Impossible d\'ajouter le commentaire !');
+    }
+    else {
+        header('Location: index.php?action=post&id=' . $postId);
+    }
 }
--- view/frontend/listPostsView.php
+++ view/frontend/listPostsView.php
@@ -18,7 +18,7 @@ while ($data = $posts->fetch())
         <p>
             <?= nl2br(htmlspecialchars($data['content'])) ?>
             <br />
-            <em>Commentaires</em>
+            <em><a href="index.php?action=post&amp;id=<?= $data['id'] ?>">Commentaires</a></em>
         </p>
     </div>
 <?php
