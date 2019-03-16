e2e9546afb69de76168feb91442025a0d4725ecb
davidcolin <davidcolin@live.fr>
2019-03-16 19:02:23 +0100

    Balise meta description

--- view/frontend/template.php
+++ view/frontend/template.php
@@ -3,9 +3,10 @@
     <head>
         <meta charset="utf-8" />
         <title><?= $title ?></title>
+        <meta name="description" content="Billet simple pour l'Alaska est un roman de Jean Forteroche publié sur le blog.">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <link href="public/css/fonts.css" rel="stylesheet" />
         <link href="public/css/frontend.css" rel="stylesheet" />
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" integrity="sha384-gfdkjb5BdAXd+lj+gudLWI+BXq4IuLW5IT+brZEZsLFm++aCMlF1V92rMkPaX4PP" crossorigin="anonymous">
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     </head>
